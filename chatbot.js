d65a8dc2f3f3d14e3c403d03cbbd3d6c625b8877
lexngzohocorp <153044918+lexngzohocorp@users.noreply.github.com>
2023-12-06 15:02:08 +0800

    Basic file upload

---
+++ chatbot.js
@@ -0,0 +1,22 @@
+function sendMessage() {
+    var userInput = document.getElementById("userInput");
+    var chatLog = document.getElementById("chatLog");
+
+    var userMessage = userInput.value;
+    if (userMessage.trim() === "") {
+        return;
+    }
+
+    // Display user message in the chat log
+    chatLog.innerHTML += "<div class='user-message'>" + userMessage + "</div>";
+
+    // Simulate a simple chatbot response
+    var botMessage = "I'm a simple chatbot!";
+    chatLog.innerHTML += "<div class='bot-message'>" + botMessage + "</div>";
+
+    // Clear the user input field
+    userInput.value = "";
+
+    // Scroll to the bottom of the chat log
+    chatLog.scrollTop = chatLog.scrollHeight;
+}
